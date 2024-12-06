12137755182da500ee2504e8615f37d2ba13a555
nilesh-0001 <castreader05@gmail.com>
2024-12-06 15:47:12 +0530

    Modified the Variables

---
+++ 02_Variables/script.js
@@ -0,0 +1,11 @@
+console.log("this session is all about variables and datatypes , syntax");
+// var a = 89; // a contains 89
+// But we have to use let in the place of var in most of the time 
+let a  = 89
+console.log(a)
+a = "nishu"
+console.log(a)
+
+// let 89Nishu = 78 // It was a error coz we cannot name any variable with a number
+
+// let var = "Nishu" // we cannot use reserved names in JavaScript 
