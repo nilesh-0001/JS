9ff4c70ff4c60c9be5d8b5394f777235ef8c4cd7
nilesh-0001 <castreader05@gmail.com>
2024-12-06 16:02:04 +0530

    Const,Let and var Update

---
+++ 03_Const,Let and Var in JS/script.js
@@ -0,0 +1,14 @@
+console.log("Difference btw var let const");
+let a = 34;
+let b = "Nishu";
+// b = 3; 
+const author = "Nilesh"
+// const nishu;
+console.log(author)
+let c = null;
+let d = undefined;
+{
+  let b = "this";
+  console.log(b);
+}
+console.log(b);
