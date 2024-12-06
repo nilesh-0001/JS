88269bd4680de4f5431e6281b85c89727ef52414
Nishu <castreader05@gmail.com>
2024-12-06 14:53:48 +0530

    Intro of Programming and JS

---
+++ 01_Intro/script.js
@@ -0,0 +1,8 @@
+console.log("First program of the javascript 😁😁🫡🫡😒");
+//Not actually first it was just a part of revison 😘😘
+
+function adding(a, b, c) {
+  return a + b + c;
+}
+let a = adding(1,3,56)
+console.log(a);
