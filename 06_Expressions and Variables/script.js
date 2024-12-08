55b1db72d3ddb005c5de5ce0df22e457f4547dfb
nilesh-0001 <castreader05@gmail.com>
2024-12-08 22:58:35 +0530

    Expression AND Operators

---
+++ 06_Expressions and Variables/script.js
@@ -0,0 +1,110 @@
+// 56;
+// "Nilesh Bisht"
+// !false
+// They all are a program also 
+
+
+// Operators in JS
+
+// Arithmetic operator
+
+let a = 5
+let b = 4
+// Addition operator
+console.log("a + b = ",a+b)
+
+// subtraction operator
+console.log("a - b = ",a-b)
+
+// division operator
+console.log("a / b = ",a/b)
+
+// multiplication operator
+console.log("a * b = ",a*b)
+
+// exponention operator
+console.log("a ** b = ",a**b)
+
+// modulus operator
+console.log("a % b = ",a%b)
+
+// decrement operator
+console.log("a-- = ",a--)
+
+// increment operator
+console.log("a++ = ",a++)
+
+// increment operator
+console.log("++a = ",++a)
+
+// decrement operator
+console.log("--a= ",--a)
+
+// Assigment operator
+
+let assignment = 1;
+assignment +=5 // same as assignment = assignment + 5
+console. log("a is now = ", assignment)
+assignment -= 5 // same as assignment = assignment - 5
+console. log( "a is now = ", assignment)
+assignment *= 5 // same as assignment = assignment * 5
+console. log( "a is now = ", assignment)
+assignment /= 5 // same as assignment = assignment / 5
+console. log("a is now = ", assignment)
+
+
+// Comparison operator
+
+let comp = 1
+let comp2 = 6
+
+// equal to
+console.log("comp == comp2 is ", comp == comp2)
+
+// not equal
+console.log("comp != comp2 is ", comp != comp2)
+
+// equal Value and type
+console.log("comp === comp2 is ", comp === comp2)
+
+// not equal Value either not equal type
+console.log("comp !== comp2 is ", comp !== comp2)
+
+// Greater than
+console.log("comp > comp2 is ", comp > comp2)
+
+
+// Logical Operator 
+
+let x = 9
+let y  = 11
+
+// Logical and 
+console.log(x<y && x!=y)
+
+//Logical or
+console.log(x>y || x!=y)
+
+// Logical not 
+console.log(!false) //It was gonna change the false into true
+console.log(!true) //It was gonna change the false into false
+
+// 9 + 7 = 15 
+
+// 9 and 7 both are operands 
+// and the signs like + = are operators 
+
+
+
+//USECASE OF COMMENTS
+// Comment in Javascript are very useful it is used when you want to add any text in your code but dont want the text to be a part of the code 
+
+// it is used to make a single line comment in js for applying this use [cntrl + /] 
+
+/* 
+I am a Good boy 
+my name is Nishu 
+and also a asspirant 
+
+*/ 
+// it was used for multiline comment 
