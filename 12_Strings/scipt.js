d8caa03283a269601d9b7e1676b9b48c982f8659
nilesh-0001 <castreader05@gmail.com>
2024-12-11 16:01:45 +0530

    Strings

---
+++ 12_Strings/scipt.js
@@ -0,0 +1,36 @@
+// // In this Session we are gonna talk about the strings
+
+let names = "Nilesh"
+console.log(names,names.length)
+
+// We can also make string by single quote '' 
+let buddy = 'Sera'
+console.log(buddy , buddy.length)
+console.log(buddy[0])
+console.log(buddy[1])
+console.log(buddy[2])
+console.log(buddy[3])
+
+// Template literals
+// let friend1 = Irus
+// let friend2 = Nishu
+
+// let sentence = `${friend1} is a Friend of ${friend2}`
+// console.log(sentence)
+
+
+// when we use backtick in the string we can also add variables in it directly . This is called String Interpulation .
+
+// Escape Sequence Characters 
+
+// for double quote 
+let fruit = "Bana\"na"
+console.log(fruit)
+console.log(fruit.length)
+
+// For single quote 
+// let fruit = 'Bana\'na'
+// console.log(fruit)
+// console.log(fruit.length)
+
+
