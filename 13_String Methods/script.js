fc279a605400694fde72ff63a88c2aa6f69daffb
nilesh-0001 <castreader05@gmail.com>
2024-12-11 17:03:29 +0530

    String Methods

---
+++ 13_String Methods/script.js
@@ -0,0 +1,38 @@
+// // In this session we are gonna talk about the String methods 
+
+// String and Methods 
+
+let me = "Nishu"
+console.log(me)
+console.log(me.length)
+
+
+
+console.log(me.toUpperCase())
+console.log(me.toLowerCase())
+
+console.log(me.slice(1,4))
+console.log(me.slice(1))
+
+let me1 = "Nilesh Pagal"
+
+console.log(me1)
+console.log(me1.replace("Pagal" , "Hoshiyar"))
+
+console.log( me.concat(" is a friend of ", me1 , " Ok"))
+
+let friend = "    Sera    "
+console.log(friend.trim())
+
+
+// Strings are imutable in order to access the character at an index we use the following syntax 
+
+
+let names = "CAST"
+console.log(names[1])
+console.log(names[3])
+console.log(names[2])
+console.log(names[0])
+
+// Homework 
+// Use a For Loop to Print a string 
