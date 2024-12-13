172e6ec4717781d4a85139a8cca30dca68c30cd6
nilesh-0001 <castreader05@gmail.com>
2024-12-14 01:02:45 +0530

    Advance Array Methods

--- 16_Array Methods/script.js
+++ 16_Array Methods/script.js
@@ -24,5 +24,55 @@ console.log(num, r)
 
 // Add new element in the Start and give the length of the array 
 let u = num.unshift(89); // Adding the 89 in the start
-console.log(num, u)
+console.log(num, u);
+
+// Delete Operator
+// delete
+let sum = [1, 2, 3, 4, 5];
+
+delete sum[0]; // It was gonna delete the array value but not gonna affect the Array length
+console.log(sum);
+console.log(sum.length);
+
+// Join Array two or more than two
+// concat()
+let num1 = [1, 2, 3, 4, 5, 6];
+let num2 = [11, 21, 31, 41, 51, 61];
+let num3 = [71, 72, 73, 74, 75, 76];
+
+let narray = num1.concat(num2, num3); // it is Gonna add all the three arrays together
+console.log(narray);
+console.log(num1, num2, num3);
+
+// sort an Array by Alphabetically
+// sort()   // It was very important to Know briefly about this method
+let tl = [2, 11, 34, 5, 78, 64, 34, 90];
+tl.sort(); // It was gonna modify the orignal array
+console.log(tl);
+
+// Sort by Compare Function
+let compare = (a, b) => {
+  return a - b;
+};
+tl.sort(compare);
+console.log(tl);
+
+// Reverse the element
+// reverse()
+tl.reverse()
+console.log(tl)
+
+// It was used to add new items to Array 
+// splice()
+let New = [1, 2, 3, 4, 5, 6];
+New.splice(3, 3, 21, 43, 67)  
+let deletedValues = New.splice(3, 3, 21, 43, 67)  
+console.log(deletedValues)
+console.log(New)
+
+// It Cretes a new Array
+let New1 = [1, 2, 3, 4, 5, 6];
+let newnum = New1.slice(3, 5)
+console.log(newnum)
+
 
