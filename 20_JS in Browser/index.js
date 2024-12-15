f73790a91e232110a53ae0ab5fd5343114a16bc3
nilesh-0001 <castreader05@gmail.com>
2024-12-16 02:26:39 +0530

    Update_File

---
+++ 20_JS in Browser/index.js
@@ -0,0 +1,20 @@
+// In this session we are gonna talk about the JavaScript in Browser 
+
+// It was basically created for adding functionality in our Website 
+
+// Javascript ability in the browser is very limited to protect the user info and safety 
+
+// A website needs three things to stand 
+// HTML
+// CSS
+// JavaScript 
+
+// Devloper mode 
+// Every browser has some developer tools 
+// Press F12 to open Developer tools 
+
+// note("It was for the frontend Work only")
+
+// We can also run JavaScript command  in the console 
+//  or 
+// Using script tag to insert js into an Html page
