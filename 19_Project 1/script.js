e9fef46125ef3dfd562239078ab01784a54a2c3d
nilesh-0001 <castreader05@gmail.com>
2024-12-16 02:00:27 +0530

    Project 1

---
+++ 19_Project 1/script.js
@@ -0,0 +1,3 @@
+// Write a JavaScript program to generate a random number and store it in a variable . This Program is gonna take an input from the user to tell them whether the guess was correct , greater or lesser than the orignal number . 100 - (no. of Guesses) is the score of the user ,The program is exppected to be terminated once the number is guessed , Number should be between 1 - 100 .
+// Eventually after the game is over , display the (100 - No. of chances ) amd the actuall no. also .
+
