31a267b4d46a9d6184d2916c7391cf71eda97a49
nilesh-0001 <castreader05@gmail.com>
2024-12-17 13:12:08 +0530

    window object and Dom or Bom

---
+++ 24_window Bom and Dom/index.js
@@ -0,0 +1,14 @@
+// In this session we are gonna talk about the window object [Dom and Bom]
+
+// window
+// window object represent browser window and provide methods to control it It is a Global scope object
+console.log(window)
+
+// [DOM] Document object Model  
+// dom represent the page content as HTML
+document.body
+document.body.style.background = "yellow"  // It is gonna change the background color into yellow
+
+// [BOM] Browser object model
+// The browser object model represent addittional object provided by the browser for working with everything except the document.
+location.href = "https://google.com"
