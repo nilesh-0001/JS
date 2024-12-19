fa3441029ecf9bd92fb72b67bff0569169fd3719
nilesh-0001 <castreader05@gmail.com>
2024-12-19 11:19:17 +0530

    Walking i-n DOM

--- 27_Walking the Dom/index.js
+++ 27_Walking the Dom/index.js
@@ -1,4 +1,31 @@
 // In this session we are gonna talk about the DOM in Deep
+// DOM Tree refers to the HTML page 
 
+// In an HTML Page, <HTML> is at the root and HEAD and BODY are its Children,etc.
 
-                                
+// A text node is always a leaf of the tree 
+
+// Auto correction
+// Whenever we make any mistake while writing html it is gonna auto correct by the browser .
+// Also when we use table we haave to add <tbody> in it .
+
+// Important graph 
+// Walking the Dom
+
+{/* <html>
+    <head>
+        <title> Tittle of the page </title>
+    </head>
+    <body>
+
+    </body>
+</html> */}
+
+// in this html code 
+document.documentElement   // Page html tag
+
+document // It refers to both children body and head 
+
+document.body  //Refers to the html body content
+
+document.head  //Refers to the html head content 
