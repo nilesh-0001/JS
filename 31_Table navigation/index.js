730bf5f907de33b297587ef7c6c1c5524fc9c0f0
nilesh-0001 <castreader05@gmail.com>
2024-12-20 08:46:37 +0530

    Table Navigation

---
+++ 31_Table navigation/index.js
@@ -0,0 +1,16 @@
+// In this video we are gonna talk about the table navigation 
+
+// Table Element support the following properties
+
+let t = document.body.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead.firstElementChild)
+console.log(t.tFoot)
+console.log(t.tBodies)
+
+console.log(t.rows[1].rowIndex)
+
+console.log(typeof window)
+console.log(typeof document)
