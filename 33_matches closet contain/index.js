955261f4f507bf37166265ffde9274ba40bdc839
nilesh-0001 <castreader05@gmail.com>
2024-12-22 21:33:50 +0530

    match closet contains

---
+++ 33_matches closet contain/index.js
@@ -0,0 +1,12 @@
+// In this Session we are gonna talk about matches,closet,contains 
+let id1 = document.getElementById("id1")
+let sp1 = document.getElementById("sp1")
+console.log(id1)
+
+console.log(id1.matches(".class"))
+console.log(id1.matches(".box"))
+console.log(sp1.closest("#sp1"))
+console.log(id1.contains("sp1"))
+console.log(sp1.contains("sp1"))
+console.log(sp1)
+
