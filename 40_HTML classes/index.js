3967cc12cc9d73bfd1eb718d1e0f548b713fb26d
nilesh-0001 <castreader05@gmail.com>
2024-12-26 10:47:40 +0530

    HTML classes

---
+++ 40_HTML classes/index.js
@@ -0,0 +1,18 @@
+// In this session we are gonna talk about on how to handle HTML classes and change it ?
+
+// It is used for changing the previous class to the current class written in program
+first.className = "text-dark color2"
+
+// It is used for removing class
+first.classList.remove("color2")
+
+// It is used for adding class
+first.classList.add("color1")
+
+// It is used for continuous executions 
+first.classList.toggle("color2")
+
+// It is used to check that the class exist or not ?
+first.classList.contains("text-dark")
+
+
