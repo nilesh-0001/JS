b558d7a82380d5afafe2159f6324ec9c3a7cf3ce
nilesh-0001 <castreader05@gmail.com>
2024-12-26 10:19:11 +0530

    Adjacent Methods

---
+++ 39_Adjacent methods/index.js
@@ -0,0 +1,10 @@
+// In this session we are gonna talk about the Insert adjacent method in HTML / Text / Element
+
+// Some Adjacent methods
+// first.insertAdjacentHTML('beforeend', '<div class = "text">beforeend</div>')
+// first.insertAdjacentHTML('beforebegin', '<div class = "text">beforebegin</div>')
+// first.insertAdjacentHTML('afterend', '<div class = "text">afterend</div>')
+// first.insertAdjacentHTML('afterbegin', '<div class = "text">afterbegin</div>')
+
+// Node - Removal
+first.remove()
