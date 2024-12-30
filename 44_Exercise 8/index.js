eefbb61c2ad5f563e7f5486d4ab1ff0f264b1899
nilesh-0001 <castreader05@gmail.com>
2024-12-30 13:30:24 +0530

    Exercise 8

---
+++ 44_Exercise 8/index.js
@@ -0,0 +1,44 @@
+// // Problem 1
+// Write a program to show different alerts when different buttons are clicked ?
+
+/* <button onclick="alert('warning 1')">Warning1</button>
+ <button onclick="alert('warning 2')">Warning2</button>
+ <button onclick="alert('warning 3')">Warning3</button>
+ <button onclick="alert('warning 4')">Warning4</button>
+ <button onclick="alert('warning 5')">Warning5</button> */
+// // Problem 2
+// Create a website which is capable of storing bookmarks of your favouraite website using href ?
+
+// By HTML  
+
+/* <a href="https://www.google.com"><button>Google</button></a>
+ <a href="https://www.youtube.com"><button>Youtube</button></a>
+ <a href="https://getbootstrap.com/"><button>Getbootstrap</button></a> */
+
+// // Problem 3
+// repeat q2 using event listeners ?
+
+// By Event listener     
+/* let x = document.getElementById("Google").addEventListener("click", function(){
+    window.location = "https://www.google.com"
+})
+let y = document.getElementById("Youtube").addEventListener("click", function(){
+    window.location = "https://www.youtube.com"
+})
+let z = document.getElementById("Getbootstrap").addEventListener("click", function(){
+    window.location = "https://getbootstrap.com/"
+}) */
+
+
+// // Problem 4
+// Write a Javascript program to keep fetching contents of a website(Every 5 sec) ?
+
+// Out of Syllabus
+
+// // Problem 5
+// Create a glowing bulb effect using classlist toggle method in javaScript ?
+
+// setInterval(async function () {
+//     document.querySelector("#bulb").classList.toggle("bulb")
+    
+// },300)
