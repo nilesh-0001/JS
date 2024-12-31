5be450f2f86d8fdbb8fceddd6b363d3a458ce5a0
nilesh-0001 <castreader05@gmail.com>
2024-12-31 21:10:22 +0530

    CallBacks

---
+++ 46_CallBacks/index.js
@@ -0,0 +1,53 @@
+// In this Session we are gonna talk about the Synchronus Programming and AsynChronus Programming or also about the callbacks.
+
+// Synchronus Programming
+// synchronus programming are the actions that initiate and finish one by one
+
+// let a = prompt("What is Your name ?")
+// let z = prompt("What is Your age ?")
+// let x = prompt("What is Your fav Color ?")
+
+// console.log("My name is " + a + " with the age of " + z + " and Fav color is " + x)
+
+// AsynChronus Programming
+// asynchronus programming are the actions that we initiate now and they finish later Ex - setTimeout
+// console.log("Start");
+// // It was gonna set the console.log() to apperar after 5000 miliseconds
+// setTimeout(function () {
+//   console.log("It is a timeout function and appeared in 5 seconds");
+// }, 5000);
+// console.log("End");
+
+// Callbacks
+function loadscript(src, callback) {
+  let script = document.createElement("script");
+  script.src = src;
+  script.onload = function () {
+    alert("Loaded sript with SRC : " + src);
+    callback(null, src);
+  };
+  script.onerror = function () {
+    alert("Error Loading sript with SRC : " + src);
+    callback(new Error("src got some Error"));
+  };
+  document.body.append(script);
+}
+function hello(error, src) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  alert("Hello World " + src);
+}
+function Morning(error, src) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  alert("Good Morning " + src);
+}
+
+loadscript(
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+  hello
+);
