e0038dcfe44cf04f14f56eed5c8f9c6d27a5c64c
nilesh-0001 <castreader05@gmail.com>
2025-01-05 12:17:26 +0530

    Finally clause

---
+++ 56_Finally clause/index.js
@@ -0,0 +1,28 @@
+// In this session we are gonna talk about the Finally clause method
+// The Try and Catch construct may have one more code clause : finally
+// If it Exists it run in all cases;
+
+const n = ()=>{
+
+
+try{
+    let x = 34
+    // console.log(action)
+    console.log("Function executed sucessfully")
+    return
+}
+catch(error){
+    console.log("It is an Error")
+    console.log(p)
+
+}
+finally{
+    console.log("It is a Finally clause example")
+    // close the file 
+    // Exit the loop 
+    // Write tothe log file
+}
+}
+
+n();
+console.log("end")
