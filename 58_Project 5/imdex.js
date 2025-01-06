76d24c0875a9580c31cdc3fe7e36dc1339db969c
nilesh-0001 <castreader05@gmail.com>
2025-01-06 23:58:57 +0530

    Project 5

---
+++ 58_Project 5/imdex.js
@@ -0,0 +1,60 @@
+// Write a JavaScript Program to look Like a Hacker write an async Function which will simply display the following output :
+// Initializing hack Program...
+// Hacking xyz usename...
+// Username found xyz...
+// Connecting to social media handles...
+let a = [
+    "Initializing Hack Program....",
+    "Connecting to the Server....",
+    "Applying Brute Force....",
+    "100K Password tried....",
+    "Match not Found....",
+    "Another 100k Passwords tried....",
+    "Match not Found....",
+    "Another 100k Passwords tried....",
+    "Match Found....",
+    "Password : A@!23*******....",
+    "Retrieving the Facebook Initials.... ",
+    "Fetched all Data....",
+    "Connecting to the  Next Server....",
+    "Applying Brute Force....",
+    "100K Password tried",
+    "Match not Found",
+    "Another 100k Passwords tried",
+    "Match Found",
+    "Password : Q2rt$*******",
+    "Retrieving the Instagram Initials ",
+    "Fetched all Data",
+    "Connecting to the  Next Server",
+    "Applying Brute Force....",
+    "100K Password tried....",
+    "Match not Found....",
+    "Another 100k Passwords tried....",
+    "Match not Found....",
+    "Another 100k Passwords tried....",
+    "Match not Found....",
+    "Another 100k Passwords tried....",
+    "Match Found....",
+    "Password : Anshu******....",
+    "Retrieving the Phone Initials....",
+    "Fetched all Data....",
+    "Taking the access of Phone....",
+    "Access Complete....",
+    "Phone no. of user : 9311573***....",
+]
+const sleep = async (seconds)=>{
+    return new Promise((reolve,reject)=>{
+        setTimeout(()=>{
+        reolve(true)
+        },seconds * 1000)
+    })
+}
+const showhack = async (message)=>{
+   await sleep(2)
+   hack.innerHTML = hack.innerHTML + message + "<br>" + "<br>"
+// console.log(message)
+}
+(async ()=>{
+    for(let i = 0 ; i<a.length;i++)
+       await showhack(a[i])
+})()
