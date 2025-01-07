22417dc3c1b5b7783d0417901861b22e2717e585
nilesh-0001 <castreader05@gmail.com>
2025-01-07 23:56:06 +0530

    Fetch API Update

--- 59_Fetch API/index.js
+++ 59_Fetch API/index.js
@@ -1,7 +1,22 @@
-// In this Session we are gonna talk about How to Fetch API 
-let x = fetch("https://goweather.herokuapp.com/weather/Ny")
+// // In this Session we are gonna talk about How to Fetch API 
+let x = fetch("https://goweather.herokuapp.com/weather/India")
 x.then((response)=>{
+    console.log(response.status)
+    console.log(response.ok)
+    //It was for getting all the values in Object format 
     return response.json()
+    // It was for getting all the value in string format
+    // return response.text()
 }).then((value)=>{
     console.log(value)
 })
+
+// the 2nd Example of Fetching API 
+// let v = fetch("https://randomuser.me/api/")
+// v.then((response) => {
+//     console.log(response.status)
+//     console.log(response.ok)
+//     return response.json()
+//   }).then((response) => {
+//     console.log(response)
+//   })
