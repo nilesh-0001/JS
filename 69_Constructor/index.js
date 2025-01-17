d298c9a599d18752d3d9d722ee8d1a5dae074f34
nilesh-0001 <castreader05@gmail.com>
2025-01-17 23:48:51 +0530

    Constructor.update

--- 69_Constructor/index.js
+++ 69_Constructor/index.js
@@ -1,30 +1,3 @@
-// class form {
-//     constructor(user,trainid){
-//         this.name = user
-//         this.trainNumber = trainid
-//         console.log("Constructor Called...... "+ user + " " + trainid)
-//     }
-//     submit(){
-//         alert( this.name + ": Form Submitted with the train number : " + this.trainNumber)
-//     }
-//     cancel(){
-//         alert( this.name + " : Cancel Form with the train number : " + this.trainNumber)
-//     }
-// }
-
-// // Create and fill Form for Nishu and Anshu 
-// let Nishu = new form("Nilesh", 234120)
-// let Nishu1 = new form("Nilesh", 564420)
-// // Nishu.fill("Nilesh", 234120)
-// // Nishu1.fill("Nilesh", 564420)
-// let Anshu = new form("Anshul", 120340)
-// // Anshu.fill("Anshul", 120340)
-
-// Nishu.submit()
-// Nishu1.submit()
-// Anshu.submit()
-// Nishu1.cancel()
-
 // We did,t have to make a class for fill we can just use Constructor for it 
 
 class form {
