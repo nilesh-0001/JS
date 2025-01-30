08332bc38b0c01171cc821a0aba652f7f0aececf
nilesh-0001 <castreader05@gmail.com>
2025-01-30 18:34:45 +0530

    Local and Global scoped

---
+++ 79_Local and Global scoped/index.js
@@ -0,0 +1,18 @@
+let p = 45
+
+function sum (){
+    let a = 5
+    console.log(a)
+    console.log(p)
+}
+
+// JavaScript has three main types of scopes 
+// Local scope
+// Function scope 
+// Global scope
+
+// let and const are block scoped and also function
+
+sum()
+console.log(p)
+console.log(a)
