2f91e00f75e90db248545aca81ec9a718795ffb0
nilesh-0001 <castreader05@gmail.com>
2025-02-06 00:40:05 +0530

    Npm and Node Modules

---
+++ 86_Npm and Node.js Packages/index.js
@@ -0,0 +1,69 @@
+// In this session we are gonna talk about the npm and node modules 
+// 
+// We can use npm init to create package.json manually means we have to fill all the detailsby self and using npm init -y ot is gonna give the direct package.json .
+
+
+// npm intall means that the this is gonna import all the modules and Dependencies which was in package.json .
+
+//Required package
+var pdf = require("pdf-creator-node");
+var fs = require("fs");
+
+// Read HTML Template
+var html = fs.readFileSync("index.html", "utf8");
+
+var options = {
+    format: "A3",
+    orientation: "portrait",
+    border: "10mm",
+    header: {
+        height: "45mm",
+        contents: '<div style="text-align: center;">Author: Nilesh</div>'
+    },
+    footer: {
+        height: "28mm",
+        contents: {
+            first: 'Cover page',
+            2: 'Second page', // Any page number is working. 1-based index
+            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+            last: 'Last Page'
+        }
+    }
+};
+
+
+var users = [
+    {
+      name: "Shyam",
+      age: "26",
+    },
+    {
+      name: "Navjot",
+      age: "26",
+    },
+    {
+      name: "Vitthal",
+      age: "26",
+    },
+  ];
+  var document = {
+    html: html,
+    data: {
+      users: users,
+    },
+    path: "./output.pdf",
+    type: "",
+  };
+
+
+  pdf
+  .create(document, options)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+
+// Finally done 
