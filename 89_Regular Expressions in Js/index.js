b0fd898df02f1aa49d7f313eeb7bfdf5e98edd16
nilesh-0001 <castreader05@gmail.com>
2025-02-06 16:51:37 +0530

    Regular Expressions

---
+++ 89_Regular Expressions in Js/index.js
@@ -0,0 +1,6 @@
+// In this session we are gonna talk about the Regular Expressions
+
+const regex = /(Nishu){1}/gi
+
+const text = "Nishu is a very very aweasome and very aweasome nice guy."
+console.log(text.replace(regex, "VERY"))
